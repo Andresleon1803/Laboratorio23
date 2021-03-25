2fa7afece2bed5dc88f5c2bc9690d189367d8b8a
Andresleon1803 <80863732+Andresleon1803@users.noreply.github.com>
2021-03-24 20:46:55 -0500

    Add files via upload

---
+++ abn.py
@@ -0,0 +1,6 @@
+for z in range (4):
+  for y in range (5):
+    print ("z vale", z, "y  vale", y)
+
+
+
---
+++ abn2.py
@@ -0,0 +1,6 @@
+for z in range (6):
+  for y in range (2):
+    print ("z vale", z, "y  vale", y)
+
+
+
